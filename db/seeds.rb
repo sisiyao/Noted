ec046c648dacfb7465539c88f604371c8373f5d6
sisiyao <sisiyao@gmail.com>
2016-09-01 00:37:39 -0700

    added user and collection seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,27 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# *****User seeds*****
-# User.create(username:"sisiyao",password:"password")
-# User.create(username:"charleswang0",password:"password")
-# User.create(username:"sarahlee0",password:"password")
-# User.create(username:"christinechang0",password:"password")
-# User.create(username:"michaelyao0",password:"password")
-# User.create(username:"mengwu0",password:"password")
-# User.create(username:"jennyqian0",password:"password")
-# User.create(username:"yacobaannobil0",password:"password")
-# User.create(username:"faithkim0",password:"password")
-# User.create(username:"sooyeonkang0",password:"password")
+# User seeds
+User.delete_all
+User.create(username:"guest", password:"password")
+User.create(username:"sisiyao", password:"password")
+User.create(username:"charleswang0", password:"password2")
+User.create(username:"sarahlee0", password:"password3")
+User.create(username:"christinechang0", password:"password4")
+User.create(username:"michaelyao0", password:"password5")
+User.create(username:"mengwu0", password:"password6")
+User.create(username:"jennyqian0", password:"password7")
+User.create(username:"yacobaannobil0", password:"password8")
+User.create(username:"faithkim0", password:"password9")
+User.create(username:"sooyeonkang0", password:"password10")
+
+# Collection seeds
+Collection.delete_all
+Collection.create(name: "Quotes", user_id: 1)
+Collection.create(name: "Work", user_id: 1)
+Collection.create(name: "Random Thoughts", user_id: 1)
+Collection.create(name: "Dogs", user_id: 1)
+Collection.create(name: "Cats", user_id: 1)
+Collection.create(name: "Horses", user_id: 1)
+Collection.create(name: "Best Places", user_id: 1)
+Collection.create(name: "Things to Remember", user_id: 1)
