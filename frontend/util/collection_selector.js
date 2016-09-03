dca2501cfa833a3cc58428c47dcbefc26c51bb7b
sisiyao <sisiyao@gmail.com>
2016-09-03 16:40:17 -0700

    collection sorting

--- frontend/util/collection_selector.js
+++ frontend/util/collection_selector.js
@@ -4,7 +4,15 @@ const collectionsSelector = state => {
   Object.keys(collectionsObj).forEach(collectionId => {
     collectionsArray.push(collectionsObj[collectionId]);
   });
-  return collectionsArray;
+  return collectionsArray.sort((a,b) => {
+    if (a.name.toLowerCase() < b.name.toLowerCase()) {
+      return -1;
+    }
+    if (a.name.toLowerCase() > b.name.toLowerCase()) {
+      return 1;
+    }
+    return 0;
+  });
 };
 
 export default collectionsSelector;
