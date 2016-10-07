553538cb3cfc86ed6496895cc68a8872f858f145
sisiyao <sisiyao@gmail.com>
2016-10-07 16:41:00 -0700

    fixed bug in new note for timestamp

--- frontend/components/notes/note_form.jsx
+++ frontend/components/notes/note_form.jsx
@@ -121,13 +121,17 @@ class NoteForm extends React.Component {
     const dateObj = this.state.updatedAt;
     const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul',
       'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    const minutes = dateObj.getMinutes() >= 10 ? dateObj.getMinutes()
+    if (dateObj === "") {
+      return;
+    } else {
+      const minutes = dateObj.getMinutes() >= 10 ? dateObj.getMinutes()
       : `0${dateObj.getMinutes()}`;
-    const seconds = dateObj.getSeconds() >= 10 ? dateObj.getSeconds()
+      const seconds = dateObj.getSeconds() >= 10 ? dateObj.getSeconds()
       : `0${dateObj.getSeconds()}`;
-    const date = `${months[dateObj.getMonth()]} ${dateObj.getDate()},
+      const date = `${months[dateObj.getMonth()]} ${dateObj.getDate()},
       ${dateObj.getFullYear()} ${dateObj.getHours()}:${minutes}:${seconds}`;
-    return <div className="note-last-saved">Last saved {date}</div>;
+      return <div className="note-last-saved">Last saved {date}</div>;
+    }
   }
 
   noteForm () {
